c9a91c7256d901c53b70ec2874a138ed4fa5dba0
Cheng Zhi <chengzhi@Chengs-MacBook-Pro.local>
2025-04-03 13:52:29 +0800

    initial commit

---
+++ test.go
@@ -0,0 +1,70 @@
+jpackage main
+
+{ 
+Section 1
+Some text here.
+}
+
+{
+dksfj. fdfdf! f
+func Listing1 error {
+	a := 3
+	tokkguggle := true                   
+	if toggle {
+		a_/, err := getValue
+		fmt.Println
+		fmt.Println(a)
+	}
+	return nil
+}
+
+func getValue() (int, error) {
+	return 4, nil
+}
+
+<div>
+  <h1>Headider1</h1>
+  <p>Paragraph1</p>
+  <p>Paragraph2</p>
+</div>
+
+func main() {
+	Listing1()
+}
+
+Id|Name|Cuteness
+01|Puppy|Very
+dkjfkdj
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+02|Kitten|Ok
+03|Bunny|Ok
+I am a se)ntence. I am another sentence because I end with a period. I am still a sentence when ending with an exclamation point! What about question mark? I am not quite a sentence because of the hyphen - and neither semicolon ; nor colon :
+
+There is an empty line above me.
---
+++ test.md
@@ -0,0 +1,30 @@
+# first line
+
+**dfjdk** *fdd*
+
+---------
+## dfjdk
+
+> dfjdk
+>
+
+>> fd
+
+>> fjdkfj
+
+>> fd
+
+
+[haode1](www.google.com)
+:
+![alt text](image.png)
+
+
+
+>> fdd
+
+
+
+return you
+对于来说确实
+
